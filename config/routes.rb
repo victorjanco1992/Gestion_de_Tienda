c049c126f83a501514d22369fa3bf32e36fbd9a3
coryosnow <coryosnow@info.com>
2024-05-22 23:54:37 -0300

    Modificando archivos varios

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,5 @@
 Rails.application.routes.draw do
-  get 'categories/index'
-  get 'categories/new'
-  get 'categories/edit'
-  get 'categories/create'
-  get 'categories/update'
-  get 'categories/destroy'
-  #get 'home/index'
-  #devise_for :users  
-  #resources :home
-
-  #resources :home 
-  root "home#index"  
-  
-
-  
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  #get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  devise_for :users
+  root to: 'home#index'  
+  resources :categories
 end
