676843b5bd983a67ae6aadc9d7da0504e54f6c94
victorjanco1992 <victorjanco1992@gmail.com>
2024-06-19 22:53:20 -0300

    modulos creados

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Gestion_de_Tienda
+
+Proyecto "sistemas administrativo de tienda"
+
+Con lenguaje Ruby y su framework ruby on rails
+
+Grupo 17
+
+Integrantes: Castillo Albert, Janco victor
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -9,7 +9,11 @@ Rails.application.config.assets.version = "1.0"
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
+<<<<<<< HEAD
 
 Rails.application.config.assets.precompile += %w( sb-admin-2.min.css )
 Rails.application.config.assets.precompile += %w( sales.css )
 
+=======
+Rails.application.config.assets.precompile += %w( sb-admin-2.min.css )
+>>>>>>> 9b865dd25d475a11581d03d08eb455fb0adf133d
